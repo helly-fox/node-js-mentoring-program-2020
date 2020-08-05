9a0a33a71c716eff8d7684d735bcf3ef36db8697
Alena Sabaleuskaya <elena.proweb@gmail.com>
2020-08-05 15:12:47 +0300

    Update to use pipeline from streams

--- src/task2.ts
+++ src/task2.ts
@@ -1,22 +1,22 @@
 import csv from 'csvtojson';
 import fs from 'fs';
 import path from 'path';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
 
-const readableStream = fs.createReadStream(path.resolve(__dirname, '../csv/csv-example.csv'));
-const writableStream = fs.createWriteStream(path.resolve(__dirname, '../csv/output.txt'));
-const csvTransform = csv({
-  colParser: {
-    Amount: 'omit',
-  },
-  delimiter: ';',
-});
+const pipelinePromise = promisify(pipeline);
+const run = () =>
+  pipelinePromise(
+    fs.createReadStream(path.resolve(__dirname, '../csv/csv-example.csv')),
+    csv({
+      colParser: {
+        Amount: 'omit',
+      },
+      delimiter: ';',
+    }),
+    fs.createWriteStream(path.resolve(__dirname, '../csv/output.txt'))
+  ).catch((err) => {
+    throw err;
+  });
 
-readableStream
-  .on('error', (error) => {
-    throw error;
-  })
-  .pipe(csvTransform)
-  .on('error', (error) => {
-    throw error;
-  })
-  .pipe(writableStream);
+run();
